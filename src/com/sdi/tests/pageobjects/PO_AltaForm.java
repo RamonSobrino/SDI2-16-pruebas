091b867c6127e7158898abef5e568d157a6c5a17
RamonSobrino <ramonsobrinollorca@gmail.com>
2017-03-13 15:53:50 +0100

    Codigo

---
+++ src/com/sdi/tests/pageobjects/PO_AltaForm.java
@@ -0,0 +1,29 @@
+package com.sdi.tests.pageobjects;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+
+public class PO_AltaForm {
+
+	
+	
+   public void rellenaEntrada(WebDriver driver, String plogin, String ppassword)
+   {
+		WebElement nombre = driver.findElement(By.id("form-principal:login"));
+		nombre.click();
+		nombre.clear();
+		nombre.sendKeys(plogin);
+		WebElement apellidos = driver.findElement(By.id("form-principal:password"));
+		apellidos.click();
+		apellidos.clear();
+		apellidos.sendKeys(ppassword);
+			
+		//Pulsar el boton de Alta.
+		By boton = By.id("form-principal:aceppt");
+		driver.findElement(boton).click();	   
+   }
+	
+	
+	
+}
