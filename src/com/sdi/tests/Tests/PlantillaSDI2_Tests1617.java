1c8e7e26dc4ff70b5ced073776738e39f07d28f5
Fuegon <fuegon.contacto@gmail.com>
2017-03-21 10:51:41 +0100

    Pruebas varias

--- src/com/sdi/tests/Tests/PlantillaSDI2_Tests1617.java
+++ src/com/sdi/tests/Tests/PlantillaSDI2_Tests1617.java
@@ -11,6 +11,7 @@ import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxBinary;
@@ -28,6 +29,7 @@ public class PlantillaSDI2_Tests1617 {
 
 	WebDriver driver;
 	List<WebElement> elementos = null;
+	private String baseURL = "http://localhost:8280/sdi2-16";
 
 	public PlantillaSDI2_Tests1617() {
 	}
@@ -39,7 +41,7 @@ public class PlantillaSDI2_Tests1617 {
 		FirefoxBinary ffBinary = new FirefoxBinary(pathToBinary);
 		FirefoxProfile firefoxProfile = new FirefoxProfile();
 		driver = new FirefoxDriver(ffBinary, firefoxProfile);
-		driver.get("http://localhost:8280/sdi2-16");
+		driver.get(baseURL);
 		// Este código es para ejecutar con una versión instalada de Firex 46.0
 		// driver = new FirefoxDriver();
 		// driver.get("http://localhost:8180/sdi2-n");
@@ -61,7 +63,6 @@ public class PlantillaSDI2_Tests1617 {
 
 		SeleniumUtils.EsperaCargaPagina(driver, "text", "Opciones", 60);
 		SeleniumUtils.textoPresentePagina(driver, "Opciones de administrador");
-		// assertTrue(false);
 	}
 
 	// PR02: Fallo en la autenticación del administrador por introducir mal el
@@ -71,7 +72,8 @@ public class PlantillaSDI2_Tests1617 {
 		new PO_AltaForm().rellenaEntrada(driver, "admin", "admin1");
 
 		SeleniumUtils.EsperaCargaPagina(driver, "text", "Error", 60);
-		SeleniumUtils.textoPresentePagina(driver, "Error");
+		SeleniumUtils.textoPresentePagina(driver,
+				"Usuario o contraseña incorrectos");
 	}
 
 	// PR03: Fallo en la autenticación del administrador por introducir mal la
@@ -81,13 +83,32 @@ public class PlantillaSDI2_Tests1617 {
 		new PO_AltaForm().rellenaEntrada(driver, "admin1", "admin");
 
 		SeleniumUtils.EsperaCargaPagina(driver, "text", "Error", 60);
-		SeleniumUtils.textoPresentePagina(driver, "Error");
+		SeleniumUtils.textoPresentePagina(driver,
+				"Usuario o contraseña incorrectos");
 	}
 
 	// PR04: Probar que la base de datos contiene los datos insertados con
 	// conexión correcta a la base de datos.
 	@Test
 	public void prueba04() {
+		new PO_AltaForm().rellenaEntrada(driver, "admin1", "admin1");
+		WebElement restart = SeleniumUtils.EsperaCargaPagina(driver, "id",
+				"restartdb", 60).get(0);
+		// Pulsamos iniciar base de datos
+		restart.click();
+		// Comprobamos que se reincia con exito
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "Exito", 60);
+		SeleniumUtils.textoPresentePagina(driver,
+				"Base de datos reiniciada con exito");
+		// Accedemos a la lista de usuarios
+		SeleniumUtils.ClickSubopcionMenuHover(driver,
+				"form-cabecera:MenuOpciones", "form-cabecera:listar");
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "user1", 60);
+		// Comprobamos que exiten los usuarios
+		SeleniumUtils.textoPresentePagina(driver, "user1");
+		SeleniumUtils.textoPresentePagina(driver, "user2");
+		SeleniumUtils.textoPresentePagina(driver, "user3");
+		// TODO: comprobar que los usuarios tiene las tareas bien
 		assertTrue(false);
 	}
 
@@ -114,14 +135,57 @@ public class PlantillaSDI2_Tests1617 {
 	// entrar con el usuario que se desactivado.
 	@Test
 	public void prueba06() {
-		assertTrue(false);
+		new PO_AltaForm().rellenaEntrada(driver, "admin1", "admin1");
+
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "Opciones", 60);
+
+		SeleniumUtils.ClickSubopcionMenuHover(driver,
+				"form-cabecera:MenuOpciones", "form-cabecera:listar");
+
+		// Deshabilitamos el usuario 3
+		WebElement user3Disable = SeleniumUtils.EsperaCargaPagina(driver, "id",
+				"listado_form:user-table:3", 60).get(0);
+		user3Disable.click();
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "Exito", 60);
+		SeleniumUtils.textoPresentePagina(driver, "Usuario user3 desactivado");
+		SeleniumUtils.textoPresentePagina(driver, "DISABLED");
+
+		// Cerramos sesion con el administrador e intentamos entrar con user3
+		SeleniumUtils.ClickSubopcionMenuHover(driver,
+				"form-cabecera:MenuOpciones", "form-cabecera:cerrar");
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "Login", 60);
+		new PO_AltaForm().rellenaEntrada(driver, "user3", "user3");
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "Error", 60);
+		SeleniumUtils.textoPresentePagina(driver,
+				"Usuario o contraseña incorrectos");
 	}
 
 	// PR07: Cambiar el estado de un usuario a DISABLED a ENABLED. Y Y tratar de
 	// entrar con el usuario que se ha activado.
 	@Test
 	public void prueba07() {
-		assertTrue(false);
+		new PO_AltaForm().rellenaEntrada(driver, "admin1", "admin1");
+
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "Opciones", 60);
+
+		SeleniumUtils.ClickSubopcionMenuHover(driver,
+				"form-cabecera:MenuOpciones", "form-cabecera:listar");
+
+		// Habilitamos el usuario 3
+		WebElement user3Disable = SeleniumUtils.EsperaCargaPagina(driver, "id",
+				"listado_form:user-table:3", 60).get(0);
+		user3Disable.click();
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "Exito", 60);
+		SeleniumUtils.textoPresentePagina(driver, "Usuario user3 activado");
+		SeleniumUtils.textoNoPresentePagina(driver, "DISABLED");
+
+		// Cerramos sesion con el administrador e intentamos entrar con user3
+		SeleniumUtils.ClickSubopcionMenuHover(driver,
+				"form-cabecera:MenuOpciones", "form-cabecera:cerrar");
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "Login", 60);
+		new PO_AltaForm().rellenaEntrada(driver, "user3", "user3");
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "Opciones", 60);
+		SeleniumUtils.textoPresentePagina(driver, "Opciones de usuario");
 	}
 
 	// PR08: Ordenar por Login
@@ -153,61 +217,113 @@ public class PlantillaSDI2_Tests1617 {
 	// PR11: Borrar una cuenta de usuario normal y datos relacionados.
 	@Test
 	public void prueba11() {
-		WebElement registro = driver.findElement(By
+		// Registramos un usuario para borrar
+		WebElement registerLink = driver.findElement(By
 				.id("form-principal:register"));
+		registerLink.click();
+		SeleniumUtils.EsperaCargaPagina(driver, "text",
+				"Registrar nuevo usuario", 60);
+		new PO_RegisterForm().rellenaEntrada(driver, "borrame",
+				"borrame@example.org", "borrame1", "borrame1");
 
-		registro.click();
+		// Entramos como administrador
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "Login", 60);
+		new PO_AltaForm().rellenaEntrada(driver, "admin1", "admin1");
 
-		SeleniumUtils.EsperaCargaPagina(driver, "text", "Email", 60);
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "Opciones", 60);
+
+		SeleniumUtils.ClickSubopcionMenuHover(driver,
+				"form-cabecera:MenuOpciones", "form-cabecera:listar");
+
+		// Borramos usuario
+		WebElement borrame = SeleniumUtils.EsperaCargaPagina(driver, "id",
+				"listado_form:user-table:4", 60).get(1);
+		borrame.click();
+		WebElement confirm = SeleniumUtils.EsperaCargaPagina(driver, "text", "Si", 60).get(0);
+		//FIXME: No hace click bien en la confirmacion
+		Actions ac = new Actions(driver);
+		ac.click(confirm);
+		//ac.keyDown(Keys.ENTER);
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "Volver a casa", 60);
+		SeleniumUtils.textoNoPresentePagina(driver, "borrame");
 
 	}
 
 	// PR12: Crear una cuenta de usuario normal con datos válidos.
 	@Test
 	public void prueba12() {
-		WebElement registerLink = driver.findElement(By.id("form-principal:register"));
+		WebElement registerLink = driver.findElement(By
+				.id("form-principal:register"));
 		registerLink.click();
-		SeleniumUtils.EsperaCargaPagina(driver, "text", "Registrar nuevo usuario", 60);
-		new PO_RegisterForm().rellenaEntrada(driver, "testuser", "test@example.org", "testus3r", "testus3r");
-		
-		SeleniumUtils.EsperaCargaPagina(driver, "text", "Registrado correctamente", 60);
+		SeleniumUtils.EsperaCargaPagina(driver, "text",
+				"Registrar nuevo usuario", 60);
+		new PO_RegisterForm().rellenaEntrada(driver, "testuser",
+				"test@example.org", "testus3r", "testus3r");
+
+		SeleniumUtils.EsperaCargaPagina(driver, "text",
+				"Exito", 60);
 		SeleniumUtils.textoPresentePagina(driver, "Registrado correctamente");
+		
+		//Comprobar que te puedes loguear correctamente
+		new PO_AltaForm().rellenaEntrada(driver, "testuser", "testus3r");
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "Opciones", 60);
+		SeleniumUtils.textoPresentePagina(driver, "Opciones de usuario");
 	}
 
 	// PR13: Crear una cuenta de usuario normal con login repetido.
 	@Test
 	public void prueba13() {
-		WebElement registerLink = driver.findElement(By.id("form-principal:register"));
+		WebElement registerLink = driver.findElement(By
+				.id("form-principal:register"));
 		registerLink.click();
-		SeleniumUtils.EsperaCargaPagina(driver, "text", "Registrar nuevo usuario", 60);
-		new PO_RegisterForm().rellenaEntrada(driver, "user1", "test@example.org", "testus3r", "testus3r");
-		
-		SeleniumUtils.EsperaCargaPagina(driver, "text", "The login is already used", 60);
+		SeleniumUtils.EsperaCargaPagina(driver, "text",
+				"Registrar nuevo usuario", 60);
+		new PO_RegisterForm().rellenaEntrada(driver, "user1",
+				"test@example.org", "testus3r", "testus3r");
+
+		SeleniumUtils.EsperaCargaPagina(driver, "text",
+				"The login is already used", 60);
 		SeleniumUtils.textoPresentePagina(driver, "The login is already used");
 	}
 
 	// PR14: Crear una cuenta de usuario normal con Email incorrecto.
 	@Test
 	public void prueba14() {
-		WebElement registerLink = driver.findElement(By.id("form-principal:register"));
+		WebElement registerLink = driver.findElement(By
+				.id("form-principal:register"));
 		registerLink.click();
-		SeleniumUtils.EsperaCargaPagina(driver, "text", "Registrar nuevo usuario", 60);
-		new PO_RegisterForm().rellenaEntrada(driver, "wrongemail", "example.org", "testus3r", "testus3r");
-		
-		SeleniumUtils.EsperaCargaPagina(driver, "text", "El email tiene que ser <nombre>@<dominio>", 60);
-		SeleniumUtils.textoPresentePagina(driver, "El email tiene que ser <nombre>@<dominio>");
+		SeleniumUtils.EsperaCargaPagina(driver, "text",
+				"Registrar nuevo usuario", 60);
+		new PO_RegisterForm().rellenaEntrada(driver, "wrongemail",
+				"example.org", "testus3r", "testus3r");
+
+		SeleniumUtils.EsperaCargaPagina(driver, "text",
+				"El email tiene que ser <nombre>@<dominio>", 60);
+		SeleniumUtils.textoPresentePagina(driver,
+				"El email tiene que ser <nombre>@<dominio>");
 	}
 
 	// PR15: Crear una cuenta de usuario normal con Password incorrecta.
 	@Test
 	public void prueba15() {
-		WebElement registerLink = driver.findElement(By.id("form-principal:register"));
+		WebElement registerLink = driver.findElement(By
+				.id("form-principal:register"));
 		registerLink.click();
-		SeleniumUtils.EsperaCargaPagina(driver, "text", "Registrar nuevo usuario", 60);
-		new PO_RegisterForm().rellenaEntrada(driver, "wrongpass", "wrong@example.org", "con", "con");
-		
-		SeleniumUtils.EsperaCargaPagina(driver, "text", "La contraseña tiene que contener como minimo 8 caracteres y al menos una letra o un numero", 60);
-		SeleniumUtils.textoPresentePagina(driver, "La contraseña tiene que contener como minimo 8 caracteres y al menos una letra o un numero");
+		SeleniumUtils.EsperaCargaPagina(driver, "text",
+				"Registrar nuevo usuario", 60);
+		new PO_RegisterForm().rellenaEntrada(driver, "wrongpass",
+				"wrong@example.org", "con", "con");
+
+		SeleniumUtils
+				.EsperaCargaPagina(
+						driver,
+						"text",
+						"La contraseña tiene que contener como minimo 8 caracteres y al menos una letra o un numero",
+						60);
+		SeleniumUtils
+				.textoPresentePagina(
+						driver,
+						"La contraseña tiene que contener como minimo 8 caracteres y al menos una letra o un numero");
 	}
 
 	// USUARIO
@@ -216,7 +332,25 @@ public class PlantillaSDI2_Tests1617 {
 	// tres páginas.
 	@Test
 	public void prueba16() {
-		assertTrue(false);
+		new PO_AltaForm().rellenaEntrada(driver, "user1", "user1");
+
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "Opciones", 60);
+		SeleniumUtils.ClickSubopcionMenuHover(driver, "form-cabecera:MenuOpciones", "form-cabecera:listar");
+		
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "Volver", 60);
+		//TODO: comprobar que son realmente las que tienen que ser
+		SeleniumUtils.textoPresentePagina(driver, "1 de 3");
+		SeleniumUtils.textoNoPresentePagina(driver, "categoria");
+		WebElement pag2 = driver.findElements(By.className("ui-paginator-page")).get(1);
+		pag2.click();
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "2 de", 60);
+		SeleniumUtils.textoPresentePagina(driver, "2 de 3");
+		SeleniumUtils.textoNoPresentePagina(driver, "categoria");
+		WebElement pag3 = driver.findElements(By.className("ui-paginator-page")).get(2);
+		pag3.click();
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "3 de", 60);
+		SeleniumUtils.textoPresentePagina(driver, "3 de 3");
+		SeleniumUtils.textoNoPresentePagina(driver, "categoria");
 	}
 
 	// PR17: Funcionamiento correcto de la ordenación por fecha planeada.
@@ -357,14 +491,24 @@ public class PlantillaSDI2_Tests1617 {
 	// autenticado como usuario normal.
 	@Test
 	public void prueba37() {
-		assertTrue(false);
+		new PO_AltaForm().rellenaEntrada(driver, "user1", "user1");
+
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "Opciones", 60);
+		
+		driver.get(baseURL+"/admin/opciones.xhtml");
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "Login", 60);
+		SeleniumUtils.textoPresentePagina(driver, "GTD Task Manager");
+		SeleniumUtils.textoPresentePagina(driver, "Login");
 	}
 
 	// PR38: Intento de acceso a un URL privado de usuario normal con un usuario
 	// no autenticado.
 	@Test
 	public void prueba38() {
-		assertTrue(false);
+		driver.get(baseURL+"/user/opciones.xhtml");
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "Login", 60);
+		SeleniumUtils.textoPresentePagina(driver, "GTD Task Manager");
+		SeleniumUtils.textoPresentePagina(driver, "Login");
 	}
 
 }
